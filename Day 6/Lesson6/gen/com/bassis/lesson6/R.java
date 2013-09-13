3df56c2c42606047a2f702d3e0338b96fdc78729
Tareq <shafiul.hasan@widespace.com>
2013-09-13 20:38:41 +0600

    Code updated for class 6

--- Day 6/Lesson6/gen/com/bassis/lesson6/R.java
+++ Day 6/Lesson6/gen/com/bassis/lesson6/R.java
@@ -8,6 +8,9 @@
 package com.bassis.lesson6;
 
 public final class R {
+    public static final class array {
+        public static final int gender_array=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,26 +27,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
-        public static final int chkRegularStudent=0x7f080006;
-        public static final int lblAge=0x7f080007;
-        public static final int lblFirstName=0x7f080000;
-        public static final int lblLastName=0x7f080002;
-        public static final int lblResult=0x7f080009;
-        public static final int lblSex=0x7f080004;
-        public static final int rtbResult=0x7f08000a;
-        public static final int skbAge=0x7f080008;
-        public static final int spnSex=0x7f080005;
-        public static final int txtFirstName=0x7f080001;
-        public static final int txtLastName=0x7f080003;
+        public static final int action_settings=0x7f09000c;
+        public static final int btnSave=0x7f09000b;
+        public static final int chkRegularStudent=0x7f090006;
+        public static final int lblAge=0x7f090007;
+        public static final int lblFirstName=0x7f090000;
+        public static final int lblLastName=0x7f090002;
+        public static final int lblResult=0x7f090009;
+        public static final int lblSex=0x7f090004;
+        public static final int rtbResult=0x7f09000a;
+        public static final int skbAge=0x7f090008;
+        public static final int spnSex=0x7f090005;
+        public static final int txtFirstName=0x7f090001;
+        public static final int txtLastName=0x7f090003;
     }
     public static final class layout {
         public static final int activity_launcher=0x7f030000;
         public static final int activity_student_registration=0x7f030001;
     }
     public static final class menu {
-        public static final int launcher=0x7f070000;
-        public static final int student_registration=0x7f070001;
+        public static final int launcher=0x7f080000;
+        public static final int student_registration=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -73,10 +77,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
---
+++ Day 6/Lesson6/src/com/bassis/lesson6/utils/Utility.java
@@ -0,0 +1,9 @@
+package com.bassis.lesson6.utils;
+
+public class Utility {
+
+    public Utility() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
