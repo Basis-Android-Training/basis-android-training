f39d5808381b701b89323695f54c1c2a747e7d81
Shafiul tareq <shafiul.hasan@widespace.com>
2013-10-20 01:43:15 +0600

    Grid view's is introduced for the next class.

---
+++ Day 9/GridViewTutorials/src/com/basis/gridviewtutorial/Luancher.java
@@ -0,0 +1,23 @@
+
+package com.basis.gridviewtutorial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Luancher extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luancher);
+        gotoStartUpActivity();
+    }
+
+    private void gotoStartUpActivity() {
+        Intent intent = new Intent(this, CustomAdapterGridView.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
